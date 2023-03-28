e53b60c432b4dcc99ba9d56104b0ef65de45798e
Adam Brunn <AdamMcAdamson@gmail.com>
2023-03-28 16:41:06 -0500

    create basic key setup

--- controllers/key.go
+++ controllers/key.go
@@ -1,9 +1,16 @@
 package controllers
 
 import (
+	"context"
+	"math/rand"
 	"net/http"
+	"time"
 
+	"github.com/UTDNebula/kms/configs"
+	"github.com/UTDNebula/kms/models"
 	"github.com/UTDNebula/kms/responses"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +18,71 @@ import (
 // Create Basic Key
 func CreateBasicKey() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		/*
+			POST /Create-Basic-Key
+			FROM DeveloperPortalBackend
+			{
+				UserID
+			}
+			Return:
+			{
+				Key_Mongo_OID,
+				Key,
+				Timed_Quota,
+				Usage_Remaining,
+				Key_Created,
+				Last_Modified
+			}
+		*/
+
+		var key models.Key
+		var user models.User
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		userID, err := primitive.ObjectIDFromHex(c.Query("user_id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: err.Error()})
+			return
+		}
+
+		// verify userID is valid
+		err = userCollection.FindOne(ctx, bson.M{"_id": userID}).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: err.Error()})
+			return
+		}
+
+		// @TODO: verify user has no basic keys (aggregation pipeline)
+
+		name, exists := c.GetQuery("name")
+
+		if !exists {
+			rand.Seed(time.Now().Unix())
+			ran_str := make([]byte, 12)
+			for i := range ran_str {
+				ran_str[i] = (byte)(65 + rand.Intn(25))
+			}
+			name = "key_" + string(ran_str)
+		}
+
+		key.ID = primitive.NewObjectID()
+		key.Type = "Basic"
+		key.Name = name
+		key.Quota = 200 // @TODO: Centralize values
+		key.QuotaType = "Daily"
+		key.UsageRemaining = key.Quota
+		key.CreatedAt = time.Now()
+		key.QuotaTimestamp = key.CreatedAt
+		key.UpdatedAt = key.CreatedAt
+		key.IsActive = true
+		key.Key = configs.GenerateKey()
+
+		// @TODO: Insert into DB
+
+		// @TODO: Return the key
+
 		c.JSON(http.StatusNotImplemented, responses.KeyResponse{Status: http.StatusNotImplemented, Message: "Not Implemented", Data: nil})
 	}
 }
