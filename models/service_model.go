141668bda65fd5c350ab0d47ca56e6ce0f3a6580
Adam Brunn <AdamMcAdamson@gmail.com>
2023-04-08 22:58:47 -0500

    implement custom json marshallers

--- models/service_model.go
+++ models/service_model.go
@@ -1,8 +1,10 @@
 package models
 
 import (
+	"encoding/json"
 	"time"
 
+	"github.com/UTDNebula/kms/configs"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -14,4 +16,18 @@ type Service struct {
 	CreatedAt         time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt         time.Time          `json:"updated_at" bson:"updated_at"`
 	SourceIdentifiers []string           `json:"source_identifiers" bson:"source_identifiers"`
+}
+
+func (s Service) MarshalJSON() ([]byte, error) {
+	type Alias Service
+	return json.Marshal(&struct {
+		CreatedAt string `json:"created_at"`
+		UpdatedAt string `json:"updated_at"`
+		Alias
+	}{
+		// use the desired date layout
+		CreatedAt: s.CreatedAt.Format(configs.DateLayout),
+		UpdatedAt: s.UpdatedAt.Format(configs.DateLayout),
+		Alias:     Alias(s),
+	})
 }
--- models/user_model.go
+++ models/user_model.go
@@ -1,8 +1,10 @@
 package models
 
 import (
+	"encoding/json"
 	"time"
 
+	"github.com/UTDNebula/kms/configs"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -16,4 +18,18 @@ type User struct {
 	BasicKey       primitive.ObjectID   `json:"basic_key" bson:"basic_key"`
 	AdvancedKeys   []primitive.ObjectID `json:"advanced_keys" bson:"advanced_keys"`
 	Services       []primitive.ObjectID `json:"services,omitempty" bson:"services,omitempty"` // @TODO: Verify best solution. Services should only appear if you are a Lead.
+}
+
+func (u User) MarshalJSON() ([]byte, error) {
+	type Alias User
+	return json.Marshal(&struct {
+		CreatedAt string `json:"created_at"`
+		UpdatedAt string `json:"updated_at"`
+		Alias
+	}{
+		// use the desired date layout
+		CreatedAt: u.CreatedAt.Format(configs.DateLayout),
+		UpdatedAt: u.UpdatedAt.Format(configs.DateLayout),
+		Alias:     Alias(u),
+	})
 }
