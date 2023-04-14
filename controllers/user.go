526f914f3158b97abdeedce7cc0f861cc587e659
Adam Brunn <AdamMcAdamson@gmail.com>
2023-04-14 13:05:45 -0500

    checkin aggregation pipelines work - WIP

--- controllers/user.go
+++ controllers/user.go
@@ -66,13 +66,13 @@ func GetUserType() gin.HandlerFunc {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
 
-		objID, err := primitive.ObjectIDFromHex(c.Query("user_id"))
+		userID, err := primitive.ObjectIDFromHex(c.Query("user_id"))
 		if err != nil {
 			c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: err.Error()})
 			return
 		}
 
-		err = userCollection.FindOne(ctx, bson.M{"_id": objID}).Decode(&user)
+		err = userCollection.FindOne(ctx, bson.M{"_id": userID}).Decode(&user)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: err.Error()})
 			return
@@ -83,8 +83,88 @@ func GetUserType() gin.HandlerFunc {
 }
 
 func GetPrivilegedUserData() gin.HandlerFunc {
+	// GET /KMS-Privileged-Data
+	// FROM DeveloperPortalBackend
+	// {
+	// 	UserID
+	// }
+	// Return:
+	// {
+	// 	User_Collection * Service (Filtered by User Type) * Key_Collection (Limited)
+	// }
 	return func(c *gin.Context) {
-		c.JSON(http.StatusNotImplemented, responses.UserResponse{Status: http.StatusNotImplemented, Message: "Not Implemented", Data: nil})
+
+		var user models.User
+		var aggregationPipeline bson.A
+		var cursor *mongo.Cursor
+		var err error
+		var res map[string]interface{}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		userID, err := primitive.ObjectIDFromHex(c.Query("user_id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: err.Error()})
+			return
+		}
+
+		err = userCollection.FindOne(ctx, bson.M{"_id": userID}).Decode(&user)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				c.JSON(http.StatusNotFound, responses.KeyResponse{Status: http.StatusNotFound, Message: "error", Data: "Invalid user_id: User does not exist"})
+				return
+			}
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: err.Error()})
+			return
+		}
+
+		// Admin Aggregation Only
+		projectServiceDetails := bson.D{{Key: "$project", Value: bson.D{{Key: "services._id", Value: "$_id"}, {Key: "services.service_name", Value: "$service_name"}, {Key: "services.service_type", Value: "$service_type"}, {Key: "services.created_at", Value: "$created_at"}, {Key: "services.updated_at", Value: "$updated_at"}, {Key: "services.source_identifiers", Value: "$source_identifiers"}}}}
+
+		// Lead Aggregation Only
+		matchLead := bson.D{{Key: "$match", Value: bson.D{{Key: "_id", Value: userID}}}}
+		lookupServices := bson.D{{Key: "$lookup", Value: bson.D{{Key: "from", Value: "services"}, {Key: "localField", Value: "services"}, {Key: "foreignField", Value: "_id"}, {Key: "as", Value: "services"}}}}
+		projectServices := bson.D{{Key: "$project", Value: bson.D{{Key: "services", Value: 1}}}}
+		unwindServices := bson.D{{Key: "$unwind", Value: bson.D{{Key: "path", Value: "$services"}, {Key: "preserveNullAndEmptyArrays", Value: true}}}}
+
+		// Both Lead and Admin Aggregation
+		lookupKeys := bson.D{{Key: "$lookup", Value: bson.D{{Key: "from", Value: "keys"}, {Key: "localField", Value: "services._id"}, {Key: "foreignField", Value: "service_id"}, {Key: "as", Value: "keys"}}}}
+		projectKeys := bson.D{{Key: "$project", Value: bson.D{{Key: "keys.key", Value: 0}}}}
+		unwindKeys := bson.D{{Key: "$unwind", Value: bson.D{{Key: "path", Value: "$keys"}, {Key: "preserveNullAndEmptyArrays", Value: true}}}}
+		lookupOwner := bson.D{{Key: "$lookup", Value: bson.D{{Key: "from", Value: "users"}, {Key: "localField", Value: "keys.owner_id"}, {Key: "foreignField", Value: "_id"}, {Key: "as", Value: "owner"}}}}
+		projectOwner := bson.D{{Key: "$project", Value: bson.D{{Key: "owner.basic_key", Value: 0}, {Key: "owner.created_at", Value: 0}, {Key: "owner.updated_at", Value: 0}, {Key: "owner.basic_key", Value: 0}, {Key: "owner.advanced_keys", Value: 0}}}}
+		unwindOwner := bson.D{{Key: "$unwind", Value: bson.D{{Key: "path", Value: "$owner"}, {Key: "preserveNullAndEmptyArrays", Value: false}}}}
+		projectOwnerIntoKey := bson.D{{Key: "$project", Value: bson.D{{Key: "services._id", Value: 1}, {Key: "services.service_name", Value: 1}, {Key: "services.service_type", Value: 1}, {Key: "services.created_at", Value: 1}, {Key: "services.updated_at", Value: 1}, {Key: "services.source_identifiers", Value: 1}, {Key: "keys._id", Value: 1}, {Key: "keys.key_type", Value: 1}, {Key: "keys.name", Value: 1}, {Key: "keys.owner_id", Value: 1}, {Key: "keys.service_id", Value: 1}, {Key: "keys.quota", Value: 1}, {Key: "keys.quota_type", Value: 1}, {Key: "keys.usage_remaining", Value: 1}, {Key: "keys.quota_timestamp", Value: 1}, {Key: "keys.created_at", Value: 1}, {Key: "keys.updated_at", Value: 1}, {Key: "keys.is_active", Value: 1}, {Key: "keys.owner", Value: "$owner"}}}}
+		groupKeys := bson.D{{Key: "$group", Value: bson.D{{Key: "_id", Value: "$services._id"}, {Key: "services", Value: bson.D{{Key: "$first", Value: "$services"}}}, {Key: "keys", Value: bson.D{{Key: "$push", Value: "$keys"}}}}}}
+		projectKeysIntoService := bson.D{{Key: "$project", Value: bson.D{{Key: "services._id", Value: 1}, {Key: "services.service_name", Value: 1}, {Key: "services.service_type", Value: 1}, {Key: "services.created_at", Value: 1}, {Key: "services.updated_at", Value: 1}, {Key: "services.source_identifiers", Value: 1}, {Key: "services.keys", Value: "$keys"}}}}
+		groupServices := bson.D{{Key: "$group", Value: bson.D{{Key: "_id", Value: primitive.Null{}}, {Key: "services", Value: bson.D{{Key: "$push", Value: "$services"}}}}}}
+
+		pipelineLeadAggregation := bson.A{matchLead, lookupServices, projectServices, unwindServices, lookupKeys, projectKeys, unwindKeys, lookupOwner, projectOwner, unwindOwner, projectOwnerIntoKey, groupKeys, projectKeysIntoService, groupServices}
+
+		pipelineAdminAggregation := bson.A{projectServiceDetails, lookupKeys, projectKeys, unwindKeys, lookupOwner, projectOwner, unwindOwner, projectOwnerIntoKey, groupKeys, projectKeysIntoService, groupServices}
+
+		// Determine course of action by checking user type
+		if user.Type == "Admin" {
+			aggregationPipeline = pipelineAdminAggregation
+		} else if user.Type == "Lead" {
+			aggregationPipeline = pipelineLeadAggregation
+			if len(user.Services) == 0 {
+				c.JSON(http.StatusConflict, responses.UserResponse{Status: http.StatusConflict, Message: "error", Data: "Invalid user_id: User is not a Lead or Admin"})
+				return
+			}
+		} else {
+			c.JSON(http.StatusConflict, responses.UserResponse{Status: http.StatusConflict, Message: "error", Data: "Invalid user_id: User is not a Lead or Admin"})
+			return
+		}
+
+		cursor, err = serviceCollection.Aggregate(ctx, aggregationPipeline)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: err.Error()})
+			return
+		}
+		cursor.All(ctx, res)
+		c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: res["services"]})
 	}
 }
 
