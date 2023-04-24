f425066c950b53afb457d247a4fa18fa613b680b
Adam Brunn <AdamMcAdamson@gmail.com>
2023-04-24 14:06:03 -0500

    implement allowing of basic keys

--- models/service_model.go
+++ models/service_model.go
@@ -12,7 +12,7 @@ import (
 type Service struct {
 	ID                primitive.ObjectID `json:"_id" bson:"_id"`
 	Name              string             `json:"service_name" bson:"service_name"`
-	Type              string             `json:"service_type" bson:"service_type"` // @TODO: Enum (?) (PublicProduction, PrivateProduction, Staging)
+	Type              string             `json:"service_type" bson:"service_type"` // @TODO: Enum (?) (Basic, PublicProduction, PrivateProduction, Staging)
 	CreatedAt         time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt         time.Time          `json:"updated_at" bson:"updated_at"`
 	SourceIdentifiers []string           `json:"source_identifiers" bson:"source_identifiers"`
